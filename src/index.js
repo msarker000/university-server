4ac9c1041842f9c9cc961163ac91832043176df8
asarker <asarker@pragmafs.com>
2019-05-24 11:55:06 -0400

    update

--- src/index.js
+++ src/index.js
@@ -652,6 +652,7 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({ url }) => {
+const PORT = process.env.PORT || 4000;
+server.listen({ port: PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
